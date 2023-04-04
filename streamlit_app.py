3e9a48ddc2b9509ddc9e3633693cf6c3bab1381a
intelligentestech <chris@networkpipe.com>
2023-04-04 10:55:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,19 +9,34 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-# Set OpenAI API key
+from streamlit_chat import message
+
 openai.api_key = st.secrets["OPENAI_KEY"]
-# Define prompt and parameters
-prompt = "Hello, can you introduce yourself?"
-model_engine = "text-davinci-002"
-temperature = 0.5
-max_tokens = 100
-# Send request to the API
-response = openai.Completion.create(
-    engine=model_engine,
-    prompt=prompt,
-    temperature=temperature,
-    max_tokens=max_tokens,
-)
-# Print response text
-st.write(response.choices[0].text.strip())
+def generate_response(prompt):
+    completion=openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.6,
+    )
+    message=completion.choices[0].text
+    return message
+
+st.title("ChatGPT-like Web App")
+#storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+user_input=st.text_input("You:",key='input')
+if user_input:
+    output=generate_response(user_input)
+    #store the output
+    st.session_state['past'].append(user_input)
+    st.session_state['generated'].append(output)
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
